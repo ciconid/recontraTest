15d1aa5b497e68e8bbae7cb42f9aaa912cc2e698
cico <cico@localhost.localdomain>
2024-06-29 07:31:46 -0300

    segundofono ramirez

--- suganda.py
+++
@@ -1,8 +0,0 @@
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas dolor, 
-nec dignissim metus. Donec augue elit, rhoncus ac sodales id, porttitor vitae est. 
-Donec laoreet rutrum libero sed pharetra.
-
- Donec vel egestas dolor, nec dignissim metus. 
- Donec augue elit, rhoncus ac sodales id, porttitor vitae est. Donec 
- laoreet rutrum libero sed pharetra. Duis a arcu convallis, gravida purus eget, 
- mollis diam.
